d38e6c13e16e64e55517f23ac2abf9600992e4a6
Manoj Kumar Munda <mk88365@gmail.com>
2023-05-08 10:26:37 +0530

    jsx is used

--- NamasteReact/script.js
+++ NamasteReact/script.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const heading = React.createElement('h1', {id : "heading"}, "I am a h1 tag");
+// const heading = React.createElement('h1', {id : "heading"}, "I am a h1 tag");
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(jsxHeading);
